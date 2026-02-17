5975fc3056c78dd7d7113cfd34901e257545b10f
Paskkal.IO <74718314+Pasify@users.noreply.github.com>
2026-02-17 21:51:03 +0000

    initial commit and basic set up of dashboard

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const interFont = Inter({
+  subsets: ["latin"],
+});
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
